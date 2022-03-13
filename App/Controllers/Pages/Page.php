024cd61ceb33fbd4e26f80bb2fc8c760b6ae3b8b
Rafael Techio <techiorafael@gmail.com>
2022-03-13 02:44:58 -0300

    tirando css da página html

--- App/Controllers/Pages/Page.php
+++ App/Controllers/Pages/Page.php
@@ -9,6 +9,9 @@ class Page {
      * Método responsável por retornar o conteúdo (view) da nossa página genérica
      */
     public static function getPage(array $params = []) :string {
+        var_dump(
+            array_merge(['/Resources/css/global.css'], $params["css"] ?? [])
+        );
         return View::render("pages/page", [
             "title" => $params["title"] ?? 'Blog Agropesca',
             "metaDescription" => $params["metaDescription"] ?? "O maior Blog de Agropesca do Oeste Paranaense",
@@ -17,7 +20,7 @@ class Page {
             "header" => self::getHeader($params),
             "footer" => $params["footer"] ?? '',
 
-            "css" => self::renderCss(array_merge($params["css"] ?? [], ['/Resources/css/global.css'])),
+            "css" => self::renderCss(array_merge(['/Resources/css/global.css'], $params["css"] ?? [])),
             "headScripts" => self::renderJs(array_merge($params["headScripts"] ?? [], (!!$params['navbar'] ?? false) ? ['https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', '/Resources/js/navbar.js'] : [])),
             "endBodyScripts" => self::renderJs($params["endBodyScripts"] ?? []),
         ]);
