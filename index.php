477d1c3de24107c64d5f363aece066e44b7653c5
Rafael Techio <techiorafael@gmail.com>
2022-03-14 21:28:48 -0300

    tirando rotas de Post

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ $router = new \App\Http\Router(getenv('URL'));
 
 include __DIR__ . '/Routes/Panel.php';
 include __DIR__ . '/Routes/Pages.php';
-include __DIR__ . '/Routes/Post.php';
 include __DIR__ . '/Routes/User.php';
 
 $router->run()
